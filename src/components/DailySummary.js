9d563707733695cc75fe781b0abeaaf07b5ac10a
Stephanie <stephanie.chenn1@gmail.com>
2022-01-30 21:43:41 -0500

    added welcome message, summary and buttons for home page

---
+++ src/components/DailySummary.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Summary = () => {
+  return (
+    <div className="summary">
+      <h3>You have not tracked any activity for today.</h3>
+      <h3>Click below to log your activity.</h3>
+    </div>
+  );
+};
+
+export default Summary;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
+import "./Home.css";
+import Summary from "../components/DailySummary";
 
 function Home() {
   return (
     <div className="home">
-      <h1>Home</h1>
+      <h1>Welcome, Stephanie!</h1>
+      <Summary />
+      <div class="btn-group">
+        <button className="button">Track your meal for carb count</button>
+        <button className="button">Track your glucose level</button>
+        <button className="button">Track your fitness</button>
+        <button className="button">Track your sleep</button>
+        <button className="button">Track your mood</button>
+      </div>
     </div>
   );
 }
